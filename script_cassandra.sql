5ff2a576d91d614e373b811c49af7b3e2389d8bf
Bruno Donato Banhos <bruno.donato.banhos@gmail.com>
2018-07-20 14:57:32 -0300

    Tempos de execução das duas primeiras queries

--- script_cassandra.sql
+++ script_cassandra.sql
@@ -14,3 +14,31 @@ CREATE TABLE names(
 )
 
 COPY names(state, genre, year, name, occurrences) FROM 'ALL_STATES.csv' WITH DELIMITER ',';
+
+-- Para mostrar os tempos de execucao
+tracing on;
+/*
+QUERY 1 com LIMIT 5
+-- ANTES - sem reiniciar o container
+17:05:46.141000
+17:05:46.168137
+
+-- DEPOIS - conteiner reiniciado
+17:15:45.628000
+17:15:45.827582
+
+QUERY 2 com LIMIT 5
+17:25:21.013000
+17:25:21.048273
+
+
+QUERY 1
+SELECT * FROM names WHERE name = 'Anna' GROUP BY state allow filtering;
+17:32:01.369000
+17:32:02.069252
+
+QUERY 2
+SELECT SUM(occurrences) as popularidade, name FROM names WHERE state = 'NY' GROUP BY name;
+17:38:42.087000
+17:39:01.309574
+*/
