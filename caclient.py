86fceb74a160ff6279ec8a25684f4bfbf2035da0
Mateus <mattvasc@outlook.com>
2018-07-22 13:55:13 -0300

    versão quase que final

--- caclient.py
+++ caclient.py
@@ -1,15 +1,32 @@
 import time
+import os
 from cassandra.cluster import Cluster
 
-cluster = Cluster()
+if "CASSANDRA_CLUSTER_IP" in os.environ:
+    cluster = Cluster([os.environ['CASSANDRA_CLUSTER_IP']])
+else:
+    cluster = Cluster()
 
 session = cluster.connect("trabalho")
+print("Conexão ao keyspace trabalho realizada com sucesso!")
 
+
+
+query = input("Qual busca Gostaria de realizar?\nQuery: ")
+visualizar = input("Gostaria de visualizar o resultado? (y/N): ")
 t = time.time()
-rows = session.execute("SELECT * FROM names WHERE name = 'Anna' GROUP BY state allow filtering;")
+rows = session.execute(query)
 elapsed = time.time() - t
 
-#for row in rows:
-#	print (row)
+i = 0
+if(visualizar == 'y'):
+	for row in rows:
+		i = 1 + i
+		print (row)
+else:
+	for row in rows:
+		i = 1 + i
+    	
 
-print(elapsed)
+print("Demorou " + str(elapsed) +" Segundos para realizar a busca, num total de " + str(i)+ " linhas.")
+
