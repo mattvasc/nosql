bd5bf689bfeaa13e2964c6e5af50089d7d839cfb
Carla Gama <carla.gaama@gmail.com>
2018-08-03 23:50:55 -0300

    json com header para import no elasticsearch

---
+++ scripts/csv_to_json_with_index.py
@@ -0,0 +1,17 @@
+import csv
+import json
+a = []
+id = 0
+with open('../ALL_STATES.csv', 'rU') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        state, gender, year, name, ocurrence = row[0:5]
+        a.append('{\"index\":{\"_index\":\"all_states\",\"id\":%d}\n' %id)
+        a.append('{\"state\":\"'+state+"\",\"gender\":\""+gender+"\",\"year\":"+year+",\"name\":\""+name+"\",\"occurrences\":"+ocurrence+"}\n")
+        id = id + 1
+a = ''.join(a) 
+csvfile.close()
+f = open("../ALL_STATES.json", "w")
+f.write(a)
+f.close()
+print("JSON salvo!")
