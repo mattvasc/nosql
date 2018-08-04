2b46ab0e129dc17173b465076db72ceb38ac4641
Carla Gama <carla.gaama@gmail.com>
2018-08-04 11:06:29 -0300

    script correto com indice

--- scripts/csv_to_json_with_index.py
+++ scripts/csv_to_json_with_index.py
@@ -2,16 +2,16 @@ import csv
 import json
 a = []
 id = 0
-with open('../ALL_STATES.csv', 'rU') as csvfile:
+with open('ALL_STATES.csv', 'rU') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for row in reader:
         state, gender, year, name, ocurrence = row[0:5]
-        a.append('{\"index\":{\"_index\":\"all_states\",\"id\":%d}\n' %id)
-        a.append('{\"state\":\"'+state+"\",\"gender\":\""+gender+"\",\"year\":"+year+",\"name\":\""+name+"\",\"occurrences\":"+ocurrence+"}\n")
+        a.append('{\"index\":{\"_index\":\"all_states\",\"_id\":%d}}\n' %id)
+        a.append('{\"id\":'+str(id)+',\"state\":\"'+state+"\",\"gender\":\""+gender+"\",\"year\":"+year+",\"name\":\""+name+"\",\"occurrences\":"+ocurrence+"}\n")
         id = id + 1
 a = ''.join(a) 
 csvfile.close()
-f = open("../ALL_STATES.json", "w")
+f = open("ALL_STATES.json", "w")
 f.write(a)
 f.close()
 print("JSON salvo!")
