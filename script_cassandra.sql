6f1a1001dd3e16452cdec35afb48387949f01f8b
Mateus S. Vasconcelos <mateus.s.vasconcelos@outlook.com>
2018-08-09 22:51:55 -0300

    Subi query1

--- script_cassandra.sql
+++ script_cassandra.sql
@@ -34,12 +34,26 @@ QUERY 2 com LIMIT 5
 17:25:21.048273
 
 
-QUERY 1
-SELECT * FROM names WHERE name = 'Anna' GROUP BY state allow filtering;
+************************QUERY 1 */
+-- CRIANDO TABELA
+DROP TABLE IF EXISTS busca1;
+CREATE TABLE busca1(
+    state TEXT,
+    genre TEXT,
+    year INT,
+    name TEXT,
+    occurrences  INT,
+		PRIMARY KEY((name), occurrences)
+);
+-- POVOANDO TABELA
+COPY busca1(state,genre,year,name,occurrences) FROM 'sample.csv';
+--      query:
+SELECT state, occurrences FROM pname3 WHERE name = 'Anne' ORDER BY occurrences DESC LIMIT 1;
+/*
 17:32:01.369000
 17:32:02.069252
 
-QUERY 2
+************************QUERY 2 *
 SELECT SUM(occurrences) as popularidade, name FROM names WHERE state = 'NY' GROUP BY name;
 17:38:42.087000
 17:39:01.309574
